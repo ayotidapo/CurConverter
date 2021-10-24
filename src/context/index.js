faf14782aacef1629241299ce2d89a6addc48ab1
otidapson <oladapo.o@softcom.ng>
2021-10-24 09:06:49 +0100

    delete redundant folders

--- src/context/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { createContext } from 'react';
-
-export const AccountContext = createContext(null);
-export const CardContext = createContext(null);
-export const BillContext = createContext(null);
-export const AppContext = createContext(null);
